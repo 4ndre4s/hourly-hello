7808d7db5dd405d335ba5b25a0bbaf3de38b0b78
Andreas Partsch <public@partsch.xyz>
2023-10-02 10:32:15 +0200

    feat: remove configuration option

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,11 @@
-const defaultConfiguration: GreetingConfiguration = {
-  messages: {
-    morning: 'Good morning',
-    afternoon: 'Good afternoon',
-    evening: 'Good evening'
-  }
-};
-
-let activeConfiguration: GreetingConfiguration = { ...defaultConfiguration };
-
 export function greet(currentDate?: Date): string {
   const internalDate = currentDate ?? new Date();
   const currentHour = internalDate.getHours();
   if (currentHour >= 0 && currentHour <= 11) {
-    return activeConfiguration.messages.morning;
+    return "Good morning";
   }
   if (currentHour >= 12 && currentHour <= 16) {
-    return activeConfiguration.messages.afternoon;
+    return "Hello";
   }
-  return activeConfiguration.messages.evening;
-}
-
-export interface GreetingConfiguration {
-  messages: {
-    morning: string;
-    afternoon: string;
-    evening: string;
-  },
-}
-
-export function configure(configuration: GreetingConfiguration) {
-  activeConfiguration = { ...defaultConfiguration, ...configuration };
+  return "Good evening";
 }
