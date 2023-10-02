63189bf0755c004c2da7a5e4c34514fe79279580
Andreas Partsch <public@partsch.xyz>
2023-10-02 11:22:04 +0200

    fix: adjust exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import * as greetingService from './lib/greeting.service';
 import * as i18nService from './lib/i18n.service';
 
-module.exports = {
-    greet: greetingService.greet,
-    setLanguage: i18nService.setLanguage
-};
+export const greet = greetingService.greet;
+export const setLanguage = i18nService.setLanguage;
