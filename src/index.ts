c233eac3e74d508f658d22a74332bf3537327778
Andreas Partsch <public@partsch.xyz>
2023-10-02 10:57:32 +0200

    chore: export language setting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as greetingService from './lib/greeting.service';
+import * as i18nService from './lib/i18n.service';
 
 module.exports = {
-    greet: greetingService.greet
+    greet: greetingService.greet,
+    setLanguage: i18nService.setLanguage
 };
