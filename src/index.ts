fced9eae7151ff8b43a9b9af13b02ad17cc138e4
Andreas Partsch <public@partsch.xyz>
2023-10-02 09:24:45 +0200

    feat: export function to greet

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world")
+export function greetings(currentDate?: Date) {
+    const internalDate = currentDate ?? new Date()
+    const currentHour = internalDate.getHours()
+    if (currentHour >= 0 && currentHour <= 11) {
+        return 'Good morning'
+    }
+    if (currentHour >= 12 && currentHour <= 16) {
+        return 'Good afternoon'
+    }
+    return 'Good evening'
+}
